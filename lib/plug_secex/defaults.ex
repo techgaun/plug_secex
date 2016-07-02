1f596c664fd9521eaf21f0191dd8d3af8f6244f7
techgaun <coolsamar207@gmail.com>
2016-07-02 01:04:53 -0500

    add more defaults

--- lib/plug_secex/defaults.ex
+++ lib/plug_secex/defaults.ex
@@ -5,13 +5,18 @@ defmodule PlugSecex.Defaults do
 
   @set_defaults [
     "X-Content-Type-Options": "nosniff",
-    "X-DNS-Prefetch-Control": "off"
+    "X-DNS-Prefetch-Control": "off",
+    "Strict-Transport-Security": "max-age=31536000",
+    "X-Xss-Protection": "1; mode=block",
+    "X-Frame-Options": "SAMEORIGIN",
+    "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' 'unsafe-eval'"
   ]
 
   @delete_defaults [
-    "X-Powered-By"
+    "X-Powered-By",
+    "Server"
   ]
 
   def to_set, do: @set_defaults
-
+  def to_delete, do: @delete_defaults
 end
